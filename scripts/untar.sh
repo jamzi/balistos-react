6b9f08db2ab608652a6791be65fb4883118f2d9c
Janez Čadež <janezcadez@Jernejs-Air.class.marg.si>
2017-08-15 14:25:10 +0200

    test2

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -6,5 +6,5 @@ echo "untar ..."
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
-cd /var/www/balistos&& \
+cd /var/www/balistos && \
 tar zxvf package.tgz -C . && \
